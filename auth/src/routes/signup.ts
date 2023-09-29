3eed3c5bb544de8957672518d1c3a74cfc1e4373
Krzysztof Matyga <krzysztof.matyga@noinputsignal.com>
2023-09-29 22:33:21 +0200

    auth routes

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,10 @@
 import express, { Response, Request } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
 
 
 const router = express.Router();
@@ -7,23 +12,40 @@ const router = express.Router();
 router.post('/api/users/signup', [
   body('email')
     .isEmail()
-    .withMessage('Email must be vali'),
+    .withMessage('Email must be valid'),
   body('password')
     .trim()
     .isLength({ min: 4, max: 20 })
     .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).send(errors.array());
+    throw new RequestValidationError(errors.array())
   }
 
   const { email, password } = req.body
 
-  console.log('Creating a user...');
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new BadRequestError('Email in use')
+  }
 
-  res.send({});
+  const user = User.build({ email, password });
+  await user.save();
+
+  //generate jwt
+  const userJwt = jwt.sign({
+    id: user.id,
+    email: user.email
+  }, "asdf");
+
+  //store it on session object
+  req.session = {
+    jwt: userJwt
+  };
+
+  res.status(201).send(user);
 });
 
 export { router as signupRouter };
